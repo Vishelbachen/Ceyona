db21640300e45ae0503e16d2d88073681cac365a
Vishelbachen <whosurdaddy027@gmail.com>
2026-04-18 03:50:18 +0400

    Create queue.py

---
+++ project-root/app/engine/queue.py
@@ -0,0 +1,16 @@
+import asyncio
+
+class TaskQueue:
+    def __init__(self):
+        self.queue = asyncio.Queue()
+
+    async def add(self, task):
+        await self.queue.put(task)
+
+    async def worker(self):
+        while True:
+            task = await self.queue.get()
+            try:
+                await task()
+            except:
+                pass
