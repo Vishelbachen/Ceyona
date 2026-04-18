76589d2784ae94334ce2c4a4dd811c8936a0992d
Vishelbachen <whosurdaddy027@gmail.com>
2026-04-18 20:09:13 +0400

    Update main.py

--- project-root/app/main.py
+++ project-root/app/main.py
@@ -1,11 +1,1 @@
-from fastapi import FastAPI
-from .api.webhook import router as webhook_router
-
-app = FastAPI()
-
-app.include_router(webhook_router)
-
-
-@app.get("/")
-async def health():
-    return {"status": "ok"}
+.
