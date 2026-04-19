c8dbf1293e29b1063c11dedde4b4e5845f72a132
Vishelbachen <whosurdaddy027@gmail.com>
2026-04-19 06:40:04 +0400

    Create memory_service.py

---
+++ project-root/app/memory/memory_service.py
@@ -0,0 +1,6 @@
+class MemoryService:
+    def __init__(self, store: SessionStore):
+        self.store = store
+
+    def build_context(self, user_id: str) -> list:
+        return self.store.get_context(user_id)
