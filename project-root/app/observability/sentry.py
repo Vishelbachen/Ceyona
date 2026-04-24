8b4fa64f8380f4e3e5c7d4ca338c8c6c61e31575
Vishelbachen <whosurdaddy027@gmail.com>
2026-04-25 01:58:45 +0400

    Update sentry.py

--- project-root/app/observability/sentry.py
+++ project-root/app/observability/sentry.py
@@ -2,4 +2,7 @@ import sentry_sdk
 from app.settings import settings
 
 def init_sentry():
-    sentry_sdk.init(dsn=settings.SENTRY_DSN)
+    sentry_sdk.init(
+        dsn=settings.SENTRY_DSN,
+        traces_sample_rate=1.0
+    )
