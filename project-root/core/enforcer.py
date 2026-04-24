fdd19ebb333e362d39a5b2948a2b40cd12e8b0fc
Vishelbachen <whosurdaddy027@gmail.com>
2026-04-24 13:38:00 +0400

    Delete enforcer.py

--- project-root/core/enforcer.py
+++
@@ -1,8 +0,0 @@
-class RuntimeEnforcer:
-
-    def check_call(self, caller, callee):
-        if not self.allowed(caller, callee):
-            raise ForbiddenDependency(f"{caller} -> {callee}")
-
-    def allowed(self, caller, callee):
-        return (caller, callee) in self.rules
