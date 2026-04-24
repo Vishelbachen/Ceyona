c666e356c273a85df33aa5383c214e4e212e613d
Vishelbachen <whosurdaddy027@gmail.com>
2026-04-25 01:49:29 +0400

    Update main.py

--- project-root/app/main.py
+++ project-root/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from app.bootstrap import create_app
+from observability.tracing import setup_tracing
+from observability.sentry import init_sentry
+
+init_sentry()
+setup_tracing()
 
 app = create_app()
 
