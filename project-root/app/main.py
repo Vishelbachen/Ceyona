9f604f197a74f2a6e7cb2908d18f6afdb6867435
Vishelbachen <whosurdaddy027@gmail.com>
2026-04-25 19:38:08 +0400

    Update main.py

--- project-root/app/main.py
+++ project-root/app/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from app.bootstrap import create_app
-from observability.tracing import setup_tracing
-from observability.sentry import init_sentry
-
-init_sentry()
-setup_tracing()
 
 app = create_app()
+
+
+@app.on_event("startup")
+async def startup_event():
+    from observability.tracing import setup_tracing
+    from observability.sentry import init_sentry
+
+    init_sentry()
+    setup_tracing()
+
 
 @app.get("/health")
 def health():
