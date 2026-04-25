d210afd7ff3d97fd215a2203127e49c78282042b
Vishelbachen <whosurdaddy027@gmail.com>
2026-04-25 21:13:39 +0400

    Update tracing.py

--- project-root/observability/tracing.py
+++ project-root/observability/tracing.py
@@ -1,9 +1,5 @@
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-
 def setup_tracing():
-
-    provider = TracerProvider()
-    trace.set_tracer_provider(provider)
-
-    return trace.get_tracer(__name__)
+    try:
+        from opentelemetry import trace
+    except ImportError:
+        return
