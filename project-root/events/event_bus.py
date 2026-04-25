d69ec81c6938c89246d900839464224d577272df
Vishelbachen <whosurdaddy027@gmail.com>
2026-04-25 04:32:32 +0400

    Update event_bus.py

--- project-root/events/event_bus.py
+++ project-root/events/event_bus.py
@@ -1,1 +1,12 @@
-.
+import redis
+from app.settings import settings
+
+r = redis.from_url(settings.REDIS_URL)
+
+def publish(event: dict):
+    r.publish("events", str(event))
+
+def subscribe():
+    pubsub = r.pubsub()
+    pubsub.subscribe("events")
+    return pubsub
