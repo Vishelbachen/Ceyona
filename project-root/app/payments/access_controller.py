d3927aaaffb1a4b45a888dec87476dfd0dde1c50
Vishelbachen <whosurdaddy027@gmail.com>
2026-04-25 04:22:04 +0400

    Delete access_controller.py

--- project-root/app/payments/access_controller.py
+++
@@ -1,9 +0,0 @@
-from payments.ton_client import get_balance
-from payments.pricing_engine import estimate_cost
-
-async def check_access(user_wallet: str, intent: dict):
-
-    balance = await get_balance(user_wallet)
-    cost = estimate_cost(intent)
-
-    return balance >= cost
