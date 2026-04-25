ce66d6d034c374a42545195ca9c64291efdc2688
Vishelbachen <whosurdaddy027@gmail.com>
2026-04-25 04:01:03 +0400

    Delete tracing.py

--- project-root/app/observability/tracing.py
+++
@@ -1,9 +0,0 @@
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-
-def setup_tracing():
-
-    provider = TracerProvider()
-    trace.set_tracer_provider(provider)
-
-    return trace.get_tracer(__name__)
